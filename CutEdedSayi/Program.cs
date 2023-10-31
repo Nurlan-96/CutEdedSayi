11cbcc9a8d9049547293a42e7d1b805d61dfaf01
Nurlan <nurlanea@code.edu.az>
2023-10-31 23:37:41 +0400

    upload

---
+++ CutEdedSayi/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine(count(10,11,12,13,14,15,16,17));
+int count (params int[] a)
+{int counter = 0;
+    foreach (int i in a)
+    {
+        if (i % 2 == 0)
+        {
+            counter++;
+        }
+    }
+    return counter;
+}
